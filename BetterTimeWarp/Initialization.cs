d423b3d7603489c8fdc05edfbd24270ed37c36b8
HappyFaceIndustries <creeperweirdo123@gmail.com>
2015-11-15 21:32:09 -0600

    Updated GameData and fixed spelling error

--- BetterTimeWarp/Initialization.cs
+++ BetterTimeWarp/Initialization.cs
@@ -42,7 +42,7 @@ namespace BetterTimeWarp
 					return;
 				}
 				//save the settings, so if they have been regenerated, it exsists and wont cause errors
-				BetterTimeWarp.SettingsNode.Save (KSPUtil.ApplicationRootPath + "GameData/BetterTimeWarp/settings.dat");
+				BetterTimeWarp.SettingsNode.Save (KSPUtil.ApplicationRootPath + "GameData/BetterTimeWarp/Settings.dat");
 
 				//subscribe to the events so that the settings save and the UI can hide/show
 				GameEvents.onGameStateSaved.Add (SaveSettings);
