a3c5ddc7e4aea5bb1e2a330dd993ae54a7046149
HappyFaceIndustries <creeperweirdo123@gmail.com>
2016-05-29 23:43:34 -0500

    Started Unity 5 UI integration and BetterTimeWarp 3.0

---
+++ BetterTimeWarp/BetterTimeWarp.Unity/ScaleContent.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace BetterTimeWarp.Unity
+{
+	[AddComponentMenu("UI/Scale Content")]
+	[RequireComponent(typeof(RectTransform))]
+	public class ScaleContent : MonoBehaviour
+	{
+		public RectTransform ScaledFrom;
+		public Vector2 Scale = new Vector2 (1f, 1f);
+		public Vector2 AnchorPosition = new Vector2 (0f, 1f);
+
+		private RectTransform rectTransform;
+
+		private void Start()
+		{
+			rectTransform = GetComponent<RectTransform> ();
+
+			UpdateAnchor ();
+			UpdateScale ();
+		}
+
+		private void Update()
+		{
+			UpdateScale ();
+		}
+
+		public void UpdateAnchor()
+		{
+			rectTransform.anchorMin = AnchorPosition;
+			rectTransform.anchorMax = AnchorPosition;
+		}
+		public void UpdateScale()
+		{
+			if (ScaledFrom == null || rectTransform == null)
+			{
+				return;
+			}
+
+			var scaledFromRect = ScaledFrom.rect;
+			rectTransform.sizeDelta = new Vector2 (scaledFromRect.width * Scale.x, scaledFromRect.height * Scale.y);
+		}
+
+		[ContextMenu("Update Properties (Manual)")]
+		public void UpdateProperties()
+		{
+			if(rectTransform == null)
+			{
+				rectTransform = GetComponent<RectTransform> ();
+			}
+			UpdateAnchor ();
+			UpdateScale ();
+		}
+		private void OnValidate()
+		{
+			UpdateProperties ();
+		}
+	}
+}
---
+++ BetterTimeWarp/BetterTimeWarp.Unity/UISkinApplicator.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace BetterTimeWarp.Unity
+{
+	[AddComponentMenu("KSP/UISkin Applicator")]
+	[RequireComponent(typeof(RectTransform))]
+	public class UISkinApplicator : MonoBehaviour
+	{
+		[SerializeField]
+		private UISkinElementType elementType = UISkinElementType.None;
+		public UISkinElementType ElementType
+		{
+			get
+			{
+				return elementType;
+			}
+		}
+
+		[SerializeField]
+		private ElementUseSettings useSettings = new ElementUseSettings ();
+		public ElementUseSettings UseSettings
+		{
+			get
+			{
+				return useSettings;
+			}
+		}
+
+		public ElementUIComponents PrepareUIComponents()
+		{
+			var graphic = GetComponent<Graphic> ();
+			var selectable = GetComponent<Selectable> ();
+
+			return new ElementUIComponents (graphic, selectable);
+		}
+
+		public enum UISkinElementType
+		{
+			None,
+			Window,
+			Box,
+			Button,
+			Toggle,
+			Label,
+			TextArea,
+			TextField,
+			ScrollView,
+			HorizontalScrollbar,
+			HorizontalScrollbarThumb,
+			HorizontalScrollbarLeftButton,
+			HorizontalScrollbarRightButton,
+			VerticalScrollbar,
+			VerticalScrollbarThumb,
+			VerticalScrollbarUpButton,
+			VerticalScrollbarDownButton,
+			HorizontalSlider,
+			VerticalSlider
+		}
+
+		[Serializable]
+		public class ElementUseSettings
+		{
+			//text settings
+			public bool UseStyleFont = true;
+			public bool UseStyleFontSize = true;
+			public bool UseStyleFontStyle = false;
+			public bool UseStyleRichText = false;
+			public bool UseStyleWordWrap = false;
+			public bool UseStyleTextClipping = false;
+			public bool UseNormalTextColor = true;
+			public bool UseTextColorTransitions = true;
+
+			//texture settings
+			public bool UseNormalSpriteBackground = true;
+			public bool UseSpriteTransitions = true;
+		}
+
+		public struct ElementUIComponents
+		{
+			public Graphic Graphic;
+			public Selectable Selectable;
+
+			public ElementUIComponents(Graphic graphic, Selectable selectable)
+			{
+				Graphic = graphic;
+				Selectable = selectable;
+			}
+		}
+	}
+}
