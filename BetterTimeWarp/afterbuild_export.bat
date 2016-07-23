d166de78be53408ef6e13013c32f3672c2d2d600
HappyFaceIndustries <creeperweirdo123@gmail.com>
2016-07-22 22:26:46 -0500

    Updated to 1.1.3

--- BetterTimeWarp/afterbuild_export.bat
+++ BetterTimeWarp/afterbuild_export.bat
@@ -1,2 +1,2 @@
-move "bin\Debug\BetterTimeWarp.dll" "C:\Users\Neb\000_AB\KSP\ksp_win_1.1.2_B_mod_testing\KSP_win\GameData\BetterTimeWarp\"
+move "bin\Debug\BetterTimeWarp.dll" "C:\Users\Neb\000_AB\KSP\ksp_win_1.1.3_B_mod_testing\KSP_win\GameData\BetterTimeWarp\"
 exit
--- BetterTimeWarp/afterbuild_run.bat
+++ BetterTimeWarp/afterbuild_run.bat
@@ -1,2 +1,2 @@
-start "KSP" "C:\Users\Neb\000_AB\KSP\ksp_win_1.1.2_B_mod_testing\KSP_win\KSP_x64.exe"
+start "KSP" "C:\Users\Neb\000_AB\KSP\ksp_win_1.1.3_B_mod_testing\KSP_win\KSP_x64.exe"
 exit
